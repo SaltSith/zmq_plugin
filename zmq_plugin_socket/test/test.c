14619bc5c8ebd9b34846ee82a579be92ff458d1a
SaltSith66 <SaltSith66@gmail.com>
2022-10-02 14:56:17 +0200

    cleanup tests

--- zmq_plugin_socket/test/test.c
+++ zmq_plugin_socket/test/test.c
@@ -37,23 +37,11 @@ void test_zmq_plugin_socket_send_message_NULL_MESSAGE_PTR(void) {
     TEST_ASSERT(result == 0);
 }
 
-void test_zmq_plugin_socket_send_message(void) {
-    int result = zmq_plugin_socket_init(PLUGIN_ROLE, PLUGIN_ADDR);
-    TEST_ASSERT(result == 0);
-
-    result = zmq_plugin_socket_send_message("Hello\0", strlen("Hello\0"));
-    TEST_ASSERT(result == -1);
-
-    result = zmq_plugin_socket_destroy();
-    TEST_ASSERT(result == 0);
-}
-
-
 TEST_LIST = {
 	{"test_zmq_plugin_socket_init_WRONG_ROLE", test_zmq_plugin_socket_init_WRONG_ROLE},
 	{"test_zmq_plugin_socket_init_ADDR_NULL_PTR", test_zmq_plugin_socket_init_ADDR_NULL_PTR},
 	{"test_zmq_plugin_socket_init", test_zmq_plugin_socket_init},
 	{"test_zmq_plugin_socket_destroy", test_zmq_plugin_socket_destroy},
-	{"test_zmq_plugin_socket_send_message", test_zmq_plugin_socket_send_message},
+    {"test_zmq_plugin_socket_send_message_NULL_MESSAGE_PTR", test_zmq_plugin_socket_send_message_NULL_MESSAGE_PTR},
 	{0} /* must be terminated with 0 */
 };
